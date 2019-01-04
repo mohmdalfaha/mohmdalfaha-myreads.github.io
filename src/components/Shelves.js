030fc5c1e8ce5142451c21cb99cfe3aed54d5fbe
mohmdalfaha <mohmd1919@gmail.com>
2019-01-04 00:36:53 +0000

    refactor: Passed books props down to shelves

--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -8,30 +8,34 @@ class Shelves extends Component{
                   <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                        <li >
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover"
-                             style={{
-                                width: 128,
-                                 height: 193,
-                                  backgroundImage:``
-                                   }}
-                              ></div>
-                              <div className="book-shelf-changer">
-                              <select >
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title"></div>
-                          <div className="book-authors"></div>
+                        {(this.props.books) && (
+                     this.props.books.map((book, id) =>
+                    <li key={id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover"
+                         style={{
+                            width: 128,
+                             height: 193,
+                              backgroundImage: `url(${book.imageLinks.thumbnail})`
+                               }}
+                          ></div>
+                          <div className="book-shelf-changer">
+                          <select value={book.shelf}
+                            onChange={(event) => this.props.updateShelf(book, event.target.value)}>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="none">None</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                          </select>
                         </div>
-                      </li>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors}</div>
+                    </div>
+                  </li>
+))}
                     </ol>
                   </div>
                 </div>
